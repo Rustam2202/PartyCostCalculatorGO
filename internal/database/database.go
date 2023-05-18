d97ff56bd2fd00a85fc56d52aa67095b5ffbe226
Rustam2202 <91486552+Rustam2202@users.noreply.github.com>
2023-05-18 15:13:07 +0300

    database methods and models

--- internal/database/database.go
+++ internal/database/database.go
@@ -7,6 +7,7 @@ import (
 
 	//	"party-calc/internal/person"
 	"party-calc/internal/config"
+	"party-calc/internal/database/models"
 	"party-calc/internal/logger"
 
 	_ "github.com/lib/pq"
@@ -38,19 +39,47 @@ func Open() {
 	if err != nil {
 		logger.Logger.Error("Can't create teable")
 	}
-
 }
 
-func AddPersonToDB(name string, spent, participants, eventId int) {
+func AddPerson(name string, spent, factor, eventId int) int64 {
 	stmt, err := DB.Prepare(`INSERT INTO persons (name, spent, participants, event)
 		VALUES(?,?,?,?)`)
 	if err != nil {
 		logger.Logger.Error("Statement prepare is incorrect")
 	}
-	_, err = stmt.Exec(name, spent, participants, eventId)
+	result, err := stmt.Exec(name, spent, factor, eventId)
 	if err != nil {
 		logger.Logger.Error("Couldn`t execute Insert operation")
 	}
+	personId, _ := result.LastInsertId()
+	return personId
+}
+
+func GetPerson(id int) {
+	var person models.Person
+	err := DB.QueryRow(`SELECT id, name, spent, factor FROM persons
+		WHERE id=$1`, id).Scan(&person.Id, &person.Name, &person.Spent, &person.Factor)
+	if err != nil {
+
+	}
+}
+
+func UpdatePerson(id int, name string, spent int, factor int) {
+	stmt, err := DB.Prepare(`UPDATE persons SET name=$1 spent=$2, factor=$3 WHERE id=$4`)
+	if err != nil {
+
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(name, spent, factor)
+}
+
+func DeletePerson(id int) {
+	stmt, err := DB.Prepare(`DELETE FROM persons WHERE id=$1`)
+	if err != nil {
+
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(id)
 }
 
 func AddEvent(name string, date time.Time) int64 {
@@ -65,4 +94,18 @@ func AddEvent(name string, date time.Time) int64 {
 	}
 	eventId, _ := result.LastInsertId()
 	return eventId
+}
+
+// use interface for call with id, name or time arguments
+func GetEvent(id int) int64 {
+
+	return 0
+}
+
+func UpdateEvent(id int) {
+
+}
+
+func DeleteEvent(id int) {
+
 }
--- internal/database/models/models.go
+++ internal/database/models/models.go
@@ -2,12 +2,25 @@ package models
 
 import "time"
 
-type Persons struct {
+type Person struct {
 	Id           int
 	Name         string
 	Spent        int
-	Participants int
-	EventsIds    []int
+	Factor int
+	//EventsIds    []int
+}
+
+type Event struct {
+	Id         int
+	//PersonsIds []int
+	Date       time.Time
+	Name       string
+}
+
+type PersonsAndEvents struct {
+	Id       int
+	PersonId int
+	EventId  int
 }
 
 type IndeptedToData struct {
@@ -16,11 +29,4 @@ type IndeptedToData struct {
 	CreditorId int
 	Sum        float32
 	PartyId    int
-}
-
-type Event struct {
-	Id         int
-	PersonsIds []int
-	Date       time.Time
-	Name       string
 }
